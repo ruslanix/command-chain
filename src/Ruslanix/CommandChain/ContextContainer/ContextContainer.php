455e75daf20360f302b492756ba0a8ec1061a7f4
ruslanix <ruslanix.com@gmail.com>
2014-03-28 22:10:14 +0200

    edit Context class

--- src/Ruslanix/CommandChain/ContextContainer/ContextContainer.php
+++ src/Ruslanix/CommandChain/ContextContainer/ContextContainer.php
@@ -6,4 +6,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 class ContextContainer extends ArrayCollection
 {
+    public function getOrException($key)
+    {
+        if (!$this->containsKey($key)) {
+            throw new \Exception(sprintf("[ContextContainer] Can't find element with key %s", $key));
+        }
+
+        return $this->get($key);
+    }
 }
